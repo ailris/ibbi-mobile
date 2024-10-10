1167b984fb912c225a5f46c5c7741c11ad803311
ailris <7christinewielia@gmail.com>
2024-10-10 22:43:34 +0700

    finale

--- calculator.js
+++ calculator.js
@@ -1,3 +1,13 @@
 const tambah =(a,b) => a+b;
 const konversiSuhu = c => (c*9/5)+32;
-module.exports = {tambah, konversiSuhu};
+const luasLingkaran = r => 3.14*r*r;
+function luasLingkaran2(r){
+    return 3.14*r*r;
+}
+function tambah2(a,b){
+    return a+b;
+}
+function konversiSuhu2(c){
+    return (c*9/5)+32;
+}
+module.exports = {tambah, konversiSuhu, luasLingkaran, luasLingkaran2, tambah2, konversiSuhu2};
--- index.js
+++ index.js
@@ -9,3 +9,19 @@ console.log('Hasil penjumlahan:', hasilPenjumlahan);
 const suhuCelsius = 100;
 const suhuFahrenheit = kalkulator.konversiSuhu(suhuCelsius);
 console.log(`${suhuCelsius}°C sama dengan ${suhuFahrenheit}°F`);
+
+// Menggunakan fungsi luasLingkaran
+const jariJari = 7;
+const luasLingkaran = kalkulator.luasLingkaran(jariJari);
+console.log(`Luas lingkaran dengan jari-jari ${jariJari} adalah ${luasLingkaran} cm²`);
+
+// Menggunakan fungsi luasLingkaran2
+const jariJari2 = 5;
+const luasLingkaran2 = kalkulator.luasLingkaran2(jariJari2);
+console.log(`Luas lingkaran dengan jari-jari ${jariJari2} adalah ${luasLingkaran2} cm²`);
+
+// Menggunakan fungsi tambah2
+const hasiltambah2 = kalkulator.tambah2(10, 5);
+console.log(`Hasil penjumlahan adalah ${hasiltambah2}`);
+
+
